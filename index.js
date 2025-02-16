9d013a5d6b404accffb5964ae628c9109cd00dc0
Ansh Dube <anshdube10801@gmail.com>
2025-02-16 13:30:37 +0530

    cors changes

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: '*',
+  origin: 'https://shortifymylink.netlify.app',
+  methods: ['GET', 'POST'],
   maxAge: 86400,
   preflightContinue: true,
 }));
